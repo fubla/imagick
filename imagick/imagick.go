00a671ffe7e37570c5f9bb992129fd59b69eb603
Nicholas Saarela <fubla@users.noreply.github.com>
2016-03-28 17:13:43 +0300

    removed the canonical stuff

--- imagick/imagick.go
+++ imagick/imagick.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package imagick // import "github.com/fubla/imagick/imagick"
+package imagick
 
 /*
 #cgo !no_pkgconfig pkg-config: MagickWand MagickCore
